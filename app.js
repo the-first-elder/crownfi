c9fe216006facce8473f60057820a3d22d7268c5
the-first-elder <emmanuelmayowa95@gmail.com>
2024-09-10 21:23:45 +0100

    final edits

--- app.js
+++ app.js
@@ -206,4 +206,4 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 });
 
-// forge script script/Wrapper.s.sol --broadcast --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
